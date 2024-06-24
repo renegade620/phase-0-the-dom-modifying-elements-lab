145e52e443d4475fd51aeff29b90883b33ff29d7
Frankline Were <franklinegift@gmail.com>
2024-06-24 13:54:40 +0300

    create, insert and delete DOM nodes

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const main = document.getElementById("main")//.remove();
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "<p>Frankline is the champion</p>";
